1146bd3e77b3b041e204b5b21dcd1640eafa97ec
TOPLAB <dombear@gmail.com>
2026-03-10 10:23:09 +0200

    Create WordPress theme from existing HTML layout

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer class="site-footer">
+      <div class="container pb-5">
+        
+        <div class="row">
+          <div class="col-md-4 py-3">
+            <h3 class="footer-heading mb-4">MagiBim</h3>
+            <p>Lcatibusdae. Et et etusa vitatiis eligend ucitem imetum simil et lab intio. Et eum aut iunditis am velluptatent </p>
+          </div>
+          <div class="col-md-7 ml-auto">
+            <div class="row">
+              <div class="col-md-6 py-3">
+                <h3 class="footer-heading mb-3"><a href="#">Главная</a></h3>
+                <h3 class="footer-heading mb-3"><a href="#">О компании</a></h3>
+                <h3 class="footer-heading mb-3"><a href="#">Продукты</a></h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">— MagiBIM Аудитор</a></li>
+                    <li><a href="#">— MagiBIM Координатор</a></li>
+                    <li><a href="#">— MagiBIM Cloud</a></li>
+                    <li><a href="#">— MagiBIM Инженер</a></li>
+                    <li><a href="#">— MagiBIM Эксперт</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-6 py-3">
+                <h3 class="footer-heading mb-4">Скачать</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#" class="btn btn-primary rounded text-white px-4">Презентация</a></li>
+                    <li><a href="#" class="btn btn-primary rounded text-white px-4">Программы</a></li>
+                    <li><a href="#" class="btn btn-primary rounded text-white px-4">Документация</a></li>
+                  </ul>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+
+      <!-- ==================  bottom black line ================== -->
+
+      <div class="row pt-2 mt-2 text-center footer-bottom-line">
+        <div class="col-md-12">
+          <p>Информация на сайте носит ознакомительный характер и не является публичной офертой. Компания оставляет за собой право изменять указанные на сайте технические характеристики. MagiBim &copy; <script>document.write(new Date().getFullYear());</script></a></p>
+        </div>
+      </div>
+    </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
